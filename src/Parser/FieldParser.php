5fd5b2c83ead800f9bffb70a8cadc384d45fc282
Raahul Seshadri <raahul@musictypes.com>
2013-05-26 09:05:46 +0530

    Abstracted away the parsing of the first field segment into a separate function

--- src/Parser/FieldParser.php
+++ src/Parser/FieldParser.php
@@ -4,51 +4,17 @@ class FieldParser
 {
     public function parse($line)
     {
-        // Explode the data into spaces
-        //
-        // There will be three parts:
-        // - The first part can be a name of a nameless type
-        // - The second part is the name of the field
-        // - The third part is the additional parameter to the field
-        //   that can be parsed differently depending on the field,
-        //   which is why we're keeping it unmodified for now
-        $data = explode(" ", $line, 3);
+        // Get a list of segments on the current line
+        $segments = $this->getLineSegments($line);
 
-        // See if the field is that of timestamps
-        if (strtolower($data[0]) == 'timestamps')
-        {
-            return array(
-                'type' => 'timestamps'
-            );
-        }
+        // If we don't have any segments, return a blank array
+        if (empty($segments)) return array();
 
-        // Else, it has to be a normal field
-        $name = $data[0]; // The name of the field
-        $type = $data[1]; // The type of the field
+        // Else, parse the first segment as the actual field details
+        $fieldData = $this->parseFieldData($segments[0]);
 
-        // The parameters are separated by spaces for a normal field, but by a
-        // CSV styled value for the enum type, which can be different types of
-        // strings
-        if ($type == 'enum')
-        {
-            // Remove extraneous spaces around the commas carefully
-            $parameters = preg_replace( "/\s*,\s*/", ",", $data[2] );
-
-            // Then parse it as a CSV
-            $parameters = str_getcsv( trim( $parameters ) );
-        }
-        else
-        {
-            $parameters = isset($data[2]) ? explode(" ", trim($data[2])) : array();
-        }
-
-
-        // Return the final set of parameters
-        return array(
-            'name' => $name,
-            'type' => $type,
-            'parameters' => $parameters
-        );
+        // Return the field data only for now
+        return $fieldData;
     }
 
 
@@ -131,5 +97,63 @@ class FieldParser
         }
 
         return $segments;
+    }
+
+
+    /**
+     * Parses the first segment of the field data to determine the
+     * type of the field, the name of the field, and also special
+     * handling for fields like enums
+     *
+     * @param  string $fieldSegment The first segment of the field line
+     * @return array                The name, type and additional parameters of a field
+     */
+    public function parseFieldData($fieldSegment)
+    {
+        // Explode the data into spaces
+        //
+        // There will be three parts:
+        // - The first part can be a name of a nameless type
+        // - The second part is the name of the field
+        // - The third part is the additional parameter to the field
+        //   that can be parsed differently depending on the field,
+        //   which is why we're keeping it unmodified for now
+        $data = explode(" ", $fieldSegment, 3);
+
+        // See if the field is that of timestamps
+        if (strtolower($data[0]) == 'timestamps')
+        {
+            return array(
+                'type' => 'timestamps'
+            );
+        }
+
+        // Else, it has to be a normal field
+        $name = $data[0]; // The name of the field
+        $type = $data[1]; // The type of the field
+
+        // The parameters are separated by spaces for a normal field, but by a
+        // CSV styled value for the enum type, which can be different types of
+        // strings
+        if ($type == 'enum')
+        {
+            // Remove extraneous spaces around the commas carefully
+            $parameters = preg_replace( "/\s*,\s*/", ",", $data[2] );
+
+            // Then parse it as a CSV
+            $parameters = str_getcsv( trim( $parameters ) );
+        }
+        else
+        {
+            $parameters = isset($data[2]) ? explode(" ", trim($data[2])) : array();
+        }
+
+
+        // Return the final set of parameters
+        return array(
+            'name' => $name,
+            'type' => $type,
+            'parameters' => $parameters
+        );
     }
 }
