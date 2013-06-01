1287e51d03df9e9776e361e6cc345a238f2f0fab
Raahul Seshadri <raahul@musictypes.com>
2013-06-01 10:54:07 +0530

    Added migration tests for all normal tables, sans the relational tables

--- tests/Generator/MigrationGeneratorTest.php
+++ tests/Generator/MigrationGeneratorTest.php
@@ -29,6 +29,41 @@ class MigrationGeneratorTest extends PHPUnit_Framework_TestCase
         $this->runGeneratedMigrationForTable('User', 'migration_user');
     }
 
+    public function testGeneratedMigrationContentsForPostTable()
+    {
+        $this->runGeneratedMigrationForTable('Post', 'migration_post');
+    }
+
+    public function testGeneratedMigrationContentsForImageTable()
+    {
+        $this->runGeneratedMigrationForTable('Image', 'migration_image');
+    }
+
+    public function testGeneratedMigrationContentsForRoleTable()
+    {
+        $this->runGeneratedMigrationForTable('Role', 'migration_role');
+    }
+
+    public function testGeneratedMigrationContentsForStuffTable()
+    {
+        $this->runGeneratedMigrationForTable('Stuff', 'migration_stuff');
+    }
+
+    public function testGeneratedMigrationContentsForThumbTable()
+    {
+        $this->runGeneratedMigrationForTable('Thumb', 'migration_thumb');
+    }
+
+    // public function testGeneratedMigrationContentsForRoleUserTable()
+    // {
+    //     $this->runGeneratedMigrationForTable('role_user', 'migration_role_user');
+    // }
+
+    // public function testGeneratedMigrationContentsForTUTable()
+    // {
+    //     $this->runGeneratedMigrationForTable('t_u', 'migration_t_u');
+    // }
+
     private function runGeneratedMigrationForTable($modelName, $migrationFile)
     {
         $expected = file_get_contents(__DIR__ . '/data/' . $migrationFile);
