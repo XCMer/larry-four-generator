e4d19a7d735cf3e5061ceffe517883706f5dbccb
Raahul Seshadri <raahul@musictypes.com>
2013-06-04 09:47:36 +0530

    Boostrap now uses composer autoloader for loading classes

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,5 @@
 <?php
 
-define('SRC_ROOT', __DIR__ .'/src/');
+include 'vendor/autoload.php';
 define('TEST_ROOT', __DIR__ .'/tests/');
-include SRC_ROOT . 'Parser/FieldParser.php';
-include SRC_ROOT . 'Parser/ModelDefinitionParser.php';
-include SRC_ROOT . 'Parser.php';
-include SRC_ROOT . 'Model.php';
-include SRC_ROOT . 'Migration.php';
-include SRC_ROOT . 'ModelList.php';
-include SRC_ROOT . 'MigrationList.php';
-include SRC_ROOT . 'Inflect.php';
-include SRC_ROOT . 'Exception/ParseError.php';
-include SRC_ROOT . 'Generator/MigrationGenerator.php';
-include SRC_ROOT . 'Generator/ModelGenerator.php';
 include TEST_ROOT . 'ParsedResult.php';
